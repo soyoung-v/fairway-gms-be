e1a80a7b4612855651f4455d6c162595aee7ab48
soyoung <ssssy93@icloud.com>
2026-06-17 12:36:38 +0900

    init: 프로젝트 초기 생성

---
+++ src/main/java/com/fairwaygms/fairwaygmsbe/FairwayGmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.fairwaygms.fairwaygmsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairwayGmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairwayGmsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fairwaygms/fairwaygmsbe/FairwayGmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairwaygms.fairwaygmsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairwayGmsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
